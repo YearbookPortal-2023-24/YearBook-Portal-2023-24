25db4942a42b0d2777d4c6052e6f9a7a40ce1f7f
aadarsh2904 <aadarshranjan2904@gmail.com>
2024-01-21 14:38:08 +0530

    Changes UserList

--- client/src/components/navbar/UserList.js
+++ client/src/components/navbar/UserList.js
@@ -3,8 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { LoginContext } from '../../helpers/Context';
 import axios from 'axios';
 import { useContext } from 'react';
-import './UserList.css';
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import {
   Menu,
   MenuButton,
@@ -13,12 +12,27 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons';
-// import { json } from 'react-router';
 import bcrypt from 'bcryptjs';
 
-
 const UserList = () => {
-  const { loggedin, setLoggedin, user, setUser, setLoading,  verified, setVerified, profileIcon, setProfileIcon, profile, setProfile, loadingSpinner, isStudent, setIsStudent, setUserData, userData } = useContext(LoginContext);
+  const {
+    loggedin,
+    setLoggedin,
+    user,
+    setUser,
+    setLoading,
+    verified,
+    setVerified,
+    profileIcon,
+    setProfileIcon,
+    profile,
+    setProfile,
+    loadingSpinner,
+    isStudent,
+    setIsStudent,
+    setUserData,
+    userData,
+  } = useContext(LoginContext);
 
   const [allUsers, setAllUsers] = useState([]);
   const [searchName, setSearchName] = useState('');
@@ -29,18 +43,15 @@ const UserList = () => {
 
   const navigate = useNavigate();
 
-
   const loadingSpinner2 = () => {
-    setLoading(true)
+    setLoading(true);
     const Load = async () => {
-      await new Promise((r) => setTimeout(r, 7000))
+      await new Promise((r) => setTimeout(r, 7000));
+      setLoading((loading) => !loading);
+    };
 
-      setLoading((loading) => !loading)
-    }
-
-    Load()
-  }
-
+    Load();
+  };
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -61,79 +72,99 @@ const UserList = () => {
     );
   };
 
-  
-
   return (
-    <div className="container">
-      <div className="search-bar">
+    <div className="container p-4">
+      <div className="flex justify-between mb-4">
         <input
           type="text"
           placeholder="Search by name"
           value={searchName}
           onChange={(e) => setSearchName(e.target.value)}
-          className="form-control"
+          className="flex-1 mr-4 p-2 border"
         />
         <input
           type="text"
           placeholder="Search by academic program"
           value={searchAcademicProgram}
           onChange={(e) => setSearchAcademicProgram(e.target.value)}
-          className="form-control"
+          className="flex-1 mr-4 p-2 border"
         />
         <input
           type="text"
           placeholder="Search by roll number"
           value={searchRollNo}
           onChange={(e) => setSearchRollNo(e.target.value)}
-          className="form-control"
+          className="flex-1 p-2 border"
         />
       </div>
 
-      <table className="table table-striped">
+      <table className="w-full table-auto border-collapse">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Academic Program</th>
-            <th>Roll No</th>
+            <th className="border p-2 text-center">Name</th>
+            <th className="border p-2 text-center">Academic Program</th>
+            <th className="border p-2 text-center">Roll No</th>
           </tr>
         </thead>
         <tbody>
-          {filterUsers().map((user) => (
-            <tr key={user.id}>
-              <td>
-                <button className="profile-link" onClick={(e) => {
-                                  e.preventDefault();
-                                  window.localStorage.removeItem('searchedAlumni')
-                                  setDisplay(false);
-                                  // loadingSpinner();
-                                  
-                                  
-                                  
-                                  axios.post(process.env.REACT_APP_API_URL + '/searchword', {
-                                    searchword: user.email
-                                  }).then((res) => {
-                                    setResult(res.data);
-                                    window.localStorage.setItem('searchedAlumni', JSON.stringify(res.data));
+          {filterUsers().map((user, index) => (
+            <tr
+              key={user.id}
+              className={`${
+                index % 2 === 0 ? 'bg-slate-950' : 'bg-slate-950'
+              } hover:bg-slate-500 transition-all`}
+            >
+              <td className="border p-2">
+                <button
+                  className={`profile-link text-white hover:text-blue-500	`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.localStorage.removeItem('searchedAlumni');
 
-                                  }).catch((err) => {
-                                    console.log(err)
-                                  })
-                 navigate(`/comment/${user.name}/${user.roll_no}`)
-                 loadingSpinner2();
-                }
-                
-              }>
-                   
+                    axios
+                      .post(process.env.REACT_APP_API_URL + '/searchword', {
+                        searchword: user.email,
+                      })
+                      .then((res) => {
+                        setResult(res.data);
+                        window.localStorage.setItem(
+                          'searchedAlumni',
+                          JSON.stringify(res.data)
+                        );
+                      })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+
+                    // Check if the current user is the logged-in user
+                    const isCurrentUser = user.email === profile.email;
+
+                    // Generate the correct profile link based on the user
+                    const profileLink = isCurrentUser
+                      ? `/profile/${profile.roll_no}/${profile.name}`
+                      : `/userlist/profile/${user.roll_no}/${user.name}`;
+                      
+
+                    // Use the correct profile link for navigation
+                    navigate(`/comment/${user.name}/${user.roll_no}`);
+                    loadingSpinner2();
+
+                    // If the clicked user is the logged-in user, navigate to their profile
+                    if (isCurrentUser) {
+                      navigate(profileLink);
+                      loadingSpinner2();
+                    }
+                  }}
+                >
                   {user.name}
                 </button>
               </td>
-              <td>{user.academic_program}</td>
-              <td>{user.roll_no}</td>
+              <td className="border p-2 text-center">{user.academic_program}</td>
+              <td className="border p-2 text-center">{user.roll_no}</td>
             </tr>
           ))}
         </tbody>
       </table>
-
     </div>
   );
 };
