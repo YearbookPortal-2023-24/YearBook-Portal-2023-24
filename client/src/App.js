8706e3a25edc6c4fad5945317cbb1703cf56da99
Samruddhi788 <125824587+Samruddhi788@users.noreply.github.com>
2024-01-28 23:41:59 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,273 @@
+import React, { useState, useEffect } from 'react'
+
+import './App.css'
+import Navbar from './components/navbar/navbar.jsx'
+import Cards from './components/team/Cards.jsx'
+import MakeAComment from './components/Make_a_Comment/MakeAComment'
+import SecondLogin from './components/SecondLogin/SecondLogin'
+import Fill from './components/Fill_Details/Fill'
+import Edit from './components/Edit_Profile/Edit'
+import Homepage from './components/Homepage/Homepage'
+import OtpVerification from './components/Otp Verification/otpVerification'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Error from './components/Error/Error'
+import Internet from './components/Internet/Internet'
+import alumniData from './components/navbar/akumniData.json'
+
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
+import jwt_decode from 'jwt-decode'
+import { LoginContext } from './helpers/Context'
+import axios from 'axios'
+import { Navbar_phone } from './components/Navbar_phone/Navbar_phone'
+
+const App = ({ location }) => {
+  const [user, setUser] = useState({})
+  const [loggedin, setLoggedin] = useState(false)
+  const [authData, setAuthData] = useState([])
+  const [result, setResult] = useState({})
+  const [isRegistered, setIsRegistered] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [fill, setFill] = useState(false)
+  const [profile, setProfile] = useState({})
+  const [allUsers, setAllUsuers] = useState([])
+  const [verified, setVerified] = useState(false)
+  const [profileIcon, setProfileIcon] = useState(false)
+  const [isStudent, setIsStudent] = useState(false)
+  const [userData, setUserData] = useState({
+    name: '',
+    roll_no: '',
+    academic_program: '',
+    department: '',
+    personal_email_id: '',
+    contact_details: '',
+    alternate_contact_details: '',
+    address: '',
+    current_company: '',
+    designation: '',
+    about: '',
+    question_1: '',
+    question_2: '',
+  })
+
+  //getting all alumnis from json
+  const alumniEmail = alumniData //geeting all the alumnis data
+
+  const navigate = useNavigate()
+
+  //Google authentication for IITI students
+  useEffect(() => {
+    
+    /*global google*/
+    if (window.google) {
+      google.accounts.id.initialize({
+        client_id: "971426024153-8iva32hh346i681clve32rkq2g7uu7eo.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+      })
+      google.accounts.id.renderButton(document.getElementById('google-login'), {
+        theme: 'outline',
+        size: 'medium',
+
+        width: 'large',
+      })
+    }
+  }, [])
+
+  //loading spinner function
+  const loadingSpinner = () => {
+    setLoading(true)
+    const Load = async () => {
+      await new Promise((r) => setTimeout(r, 2000))
+
+      setLoading((loading) => !loading)
+    }
+
+    Load()
+  }
+
+  //Get all users's name branch and email id
+  useEffect(() => {
+    
+    axios
+      .get(process.env.REACT_APP_API_URL + '/getUsersData')
+      .then((res) => {
+        setAllUsuers(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  //Callback Function after logging in
+  async function handleCallbackResponse(response) {
+    //getting all the data from google for the user who signs in
+    var userObject = jwt_decode(response.credential)
+    setUser(userObject)
+    setLoggedin(true)
+    loadingSpinner()
+
+    //Storing the users data in the localStorage
+    window.localStorage.setItem('user', JSON.stringify(userObject))
+    window.localStorage.setItem('loggedin', true)
+    //Rendering the signin button
+    document.getElementById('google-login').hidden = true
+    
+
+    await axios
+      .post(process.env.REACT_APP_API_URL + '/checkAuth', {
+        email: userObject.email,
+      })
+      .then((res) => {
+        //If the user already exists in the auth model
+        if (res.data.message === 'true') {
+          //if the user is a lumni
+          if (alumniEmail.includes(userObject.email)) {
+            axios
+              .post(process.env.REACT_APP_API_URL + '/findAUser', {
+                email: userObject.email,
+              })
+              .then((res) => {
+                //If the user had made his profile
+                if (res.data.message === 'User Found') {
+                  //if the user is verified
+                  if (res.data.User[0].two_step_verified === true) {
+                    setProfileIcon(true)
+                    setVerified(true)
+                    setProfile(res.data.User[0])
+                    window.localStorage.setItem('verified', true)
+                    window.localStorage.setItem('profileIcon', true)
+                    const p = JSON.stringify(res.data.User[0])
+                    window.localStorage.setItem('profile', p)
+                    navigate(`/`)
+                  }
+                  //if the user is not verified
+                  else {
+                    axios
+                      .post(process.env.REACT_APP_API_URL + '/deleteUser', {
+                        email: userObject.email,
+                      })
+                      .then((res) => {
+                        // console.log(res.data.message)
+                      })
+                      .catch((err) => {
+                        console.log(err)
+                      })
+                    navigate(`/fill/${userObject.jti}`)
+                  }
+                  // if the user has not made the profile but already exists in the auth
+                  // then navigate user to fill page
+                } else {
+                  navigate(`/fill/${userObject.jti}`)
+                }
+              })
+            
+          }
+          //If the user is student
+          else {
+            setFill(true)
+            navigate('/')
+            
+          }
+        }
+        //if signed in for the first time
+        else {
+          axios
+            .post(process.env.REACT_APP_API_URL + '/auth', {
+              email: userObject.email,
+              name: userObject.name,
+            })
+            .then((res) => {
+              // console.log(res)
+              //If alumni
+              if (alumniEmail.includes(userObject.email)) {
+                
+                navigate(`/fill/${userObject.jti}`)
+              }
+              //if student
+              else {
+                setFill(true)
+                navigate('/')
+                
+              }
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        }
+
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <LoginContext.Provider
+      value={{
+        loggedin,
+        setLoggedin,
+        user,
+        setUser,
+        authData,
+        setAuthData,
+        result,
+        setResult,
+        isRegistered,
+        setIsRegistered,
+        loading,
+        setLoading,
+        loadingSpinner,
+        fill,
+        setFill,
+        profile,
+        setProfile,
+        allUsers,
+        verified,
+        setVerified,
+        profileIcon,
+        verified,
+        setProfileIcon,
+        userData,
+        setUserData,
+        isStudent,
+        setIsStudent,
+      }}
+    >
+      <div className="App overflow-x-hidden">
+        {window.location.pathname !== '/fill/:userId' &&
+          window.location.pathname !== '/otpVerification/:userId' &&
+          window.location.pathname !== '*' && <Navbar />}
+        {/* <Navbar_phone /> */}
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/nav" element={<Navbar_phone />} />
+          <Route exact path="/fill/:userId" element={<Fill />} />
+          <Route exact path="/edit/:userId" element={<Edit />} />
+          <Route
+            exact
+            path="/profile/:_id/:name/:token"
+            element={<SecondLogin />}
+          />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/team" element={<Cards />} />
+          <Route
+            exact
+            path="/comment/:name/:roll_no"
+            element={<MakeAComment />}
+          />
+          <Route
+            exact
+            path="/otpVerification/:userId"
+            element={<OtpVerification />}
+          />
+          <Route exact path="*" element={<Error />} />
+          <Route exact path='/issue' element={<Internet/>}/>
+        </Routes>
+
+        {!loading && <Footer />}
+      </div>
+    </LoginContext.Provider>
+  )
+}
+export default App
---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,371 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+// import { useRouteLoaderData } from "react-router-dom";
+import './Navbar.scss';
+import { LoginContext } from '../../helpers/Context';
+import { useContext } from 'react';
+import axios from 'axios';
+import alumniData from './akumniData.json'
+import { motion } from 'framer-motion'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from '@chakra-ui/react';
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons';
+// import { json } from 'react-router';
+import bcrypt from 'bcryptjs';
+
+
+const Navbar = () => {
+
+  const variants = {
+    open: { y: 0, transition: {} },
+    closed: { y: "-96%" },
+  }
+
+  const { loggedin, setLoggedin, user, setUser, setLoading, allUsers, verified, setVerified, profileIcon, setProfileIcon, profile, setProfile, loadingSpinner, isStudent, setIsStudent, setUserData, userData } = useContext(LoginContext);
+
+  const navigate = useNavigate();
+  const [navOpen, setNavopen] = useState(false);
+  const [setSearchword] = useState("");
+  // const [searchword] = useState("");
+  const [wordentered, setWordentered] = useState("");
+  const [wordEnteredList] = useState([]);
+  // const [setWordEnteredList] = useState([]);
+  const { result, setResult } = useContext(LoginContext);
+  const [inputValue, setInputValue] = useState();
+  const [display, setDisplay] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const [example, setExample] = useState([]);
+  const alumniEmail = alumniData; //geeting all the alumnis data
+  const [count, setCount] = useState(0)
+
+
+  //Use ReactFilter
+  var filteredPersons = []
+  useEffect(() => {
+    filteredPersons = allUsers.filter(
+      person => {
+        return (
+          person
+            .name
+            .toLowerCase()
+            .startsWith(wordentered.toLowerCase())
+        );
+      }
+    );
+
+    setExample(filteredPersons.slice(0,7));
+  }, [wordentered])
+
+
+  //loading spinner function
+  const loadingSpinner2 = () => {
+    setLoading(true)
+    const Load = async () => {
+      await new Promise((r) => setTimeout(r, 7000))
+
+      setLoading((loading) => !loading)
+    }
+
+    Load()
+  }
+
+  //After refreshing the page user is still signed in 
+  useEffect(() => {
+
+    if (window.localStorage.getItem('user') !== null) {
+      const userLoggedIn = window.localStorage.getItem('user');
+      if (userLoggedIn !== null) {
+        setUser(JSON.parse(userLoggedIn));
+      }
+    }
+    if (window.localStorage.getItem('searchedAlumni') !== null) {
+      const salumni = window.localStorage.getItem('searchedAlumni');
+      if (salumni !== null) {
+        setResult(JSON.parse(salumni));
+      }
+    }
+    if (window.localStorage.getItem('userData') !== null) {
+      const u = window.localStorage.getItem('userData');
+      if (u !== null) {
+        setUserData(JSON.parse(u));
+      }
+    }
+    const logged = (window.localStorage.getItem('loggedin'));
+    if (logged === "true") {
+      setLoggedin(true);
+    }
+    else {
+      setLoggedin(false);
+    }
+
+    const profileI = (window.localStorage.getItem('profileIcon'));
+    if (profileI === "true") {
+      setProfileIcon(true);
+    }
+
+    const verify = (window.localStorage.getItem('verified'));
+    if (verify === 'true') {
+      setVerified(true);
+    }
+
+    const p = (window.localStorage.getItem('profile'));
+    if (verify === "true") {
+      setProfile(JSON.parse(p));
+    }
+
+  },[])
+
+  // console.log(loggedin);
+  // console.log(verified);
+
+  //Logout Function
+  const handleLogout = () => {
+    setUser({});
+    navigate('/');
+    window.location.reload();
+    window.localStorage.removeItem('user');
+    window.localStorage.removeItem('searchAlumni');
+    window.localStorage.removeItem('profileIcon');
+    window.localStorage.removeItem('verified');
+    window.localStorage.removeItem('profile')
+    setLoggedin(false);
+    setProfileIcon(false);
+    window.localStorage.removeItem('searchedAlumni');
+    window.localStorage.removeItem('userData');
+    window.localStorage.setItem('loggedin', false)
+    window.localStorage.removeItem('loggedin')
+    document.getElementId("google-login").hidden = false;
+
+    // console.log('logout');
+
+
+  }
+
+  //adding sidebar on smaller screens
+  const handleNavbar = () => {
+    setNavopen(!navOpen)
+  };
+  const handleDropdownclick = (e) => {
+    e.stopPropagation();
+  };
+  const handleNavopen = () => {
+    if (navOpen) {
+      setNavopen(!navOpen)
+    }
+  };
+  const renderNav = () => {
+    if (navOpen) {
+      return "links active"
+    } else {
+      return "links deactivate";
+    }
+  }
+
+
+  // Token Generation 
+
+  const token = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  };
+
+  if (loggedin === true) {
+    document.getElementById("google-login").hidden = true;
+  }
+
+
+
+  if (alumniEmail.includes(user.email)) {
+    setIsStudent(false);
+  }
+  else {
+    setIsStudent(true);
+  }
+
+  const searchAWord = (event) => {
+    setWordentered(event.target.value);
+    setInputValue(event.target.value);
+  }
+
+  return (
+    <>
+      <div className='navbar-laptop'>
+        <div className="overflow-x-hidden" id='abd'>
+          <style>
+            @import url('https://fonts.googleapis.com/css2?family=Quantico&display=swap');
+          </style>
+          <div className='header22'>
+            <img src='/images/1.png' alt='err' />
+            <div className='navbar'>
+              <ul>
+                <div onClick={handleNavopen} className={renderNav()}>
+                  <Link id='av' to="/">HOME</Link>
+                  <Link id='av' to="/about">ABOUT</Link>
+                  <Link id='av' to="/team">DEVELOPERS</Link>
+                  {loggedin &&
+                    <>
+                      {(profileIcon) ?
+                        <div id='contain'>
+                        <Menu>
+                          <MenuButton as={Button} w='29%' ml={2} rightIcon={<ChevronDownIcon />}>
+                            <img src="../../../images/profile.jpg" alt="" id='profilepic' />
+                          </MenuButton>
+                          <MenuList>
+                            <><Link id='avl' to={`profile/${profile._id}/${profile.name}/${token(32)}`}>
+                              <MenuItem id='avl' bgColor={'#4d1a6c'}>My Profile</MenuItem></Link></>
+                            <MenuItem bgColor={'#4d1a6c'} onClick={handleLogout}>Sign Out</MenuItem>
+                          </MenuList>
+                        </Menu>
+                        </div> :
+                        <>
+                        <button id='logout' onClick={handleLogout}>Sign Out</button>
+                        
+                        
+                        </>
+                      }
+                    </>}
+                  {loggedin && profileIcon && <><button 
+                  className={renderNav()} onClick={handleLogout}>SIGN OUT</button></>}
+                  {loggedin && profileIcon && <button id='prof' className={renderNav()}><Link to={`profile/${profile._id}/${profile.name}/${token(32)}`}>PROFILE</Link></button>}
+                </div>
+
+                <div onClick={handleNavbar} className="hamburger-toggle">
+                  <HamburgerIcon />
+                </div>
+
+                <div id='google-login'>
+                </div>
+
+                <>
+                  {loggedin &&
+                    <div id='loggedIn'>
+                      <li className="dropdown-nav" onClick={handleDropdownclick} style={{ display: 'flex' }}>
+                        <div className="searchr" style={{ width: '70%', display: "flex" }}>
+                          {(isStudent || verified) && <>
+                            <input type="text" placeholder="Search..." class="search nav-keliye" onChange={(e) => {
+                              searchAWord(e);
+                              (e.target.value === "") ? setDisplay(false) : setDisplay(true);
+                              // onEnter();
+                            }} value={inputValue} />
+                            {wordEnteredList.length === undefined &&
+                              <ul>
+                                <li><button className={`btnsearch2 ${(display) ? "" : "display-none"}`} style={{ textAlign: "left" }}>No User Found</button></li>
+                              </ul>}
+                            {example.length !== 0 &&
+                              <ul className='names'>
+                                {example.map((val, index) =>
+                                (<li><button className={`btnsearch2 ${(display) ? "" : "display-none"}`} style={{ textAlign: "left" }} key={index} onClick={(e) => {
+                                  e.preventDefault();
+                                  window.localStorage.removeItem('searchedAlumni')
+                                  // loadingSpinner();
+                                  setInputValue("");
+                                  setDisplay(false);
+                                  e.target.value = "";
+                                  axios.post(process.env.REACT_APP_API_URL + '/searchword', {
+                                    searchword: val.email
+                                  }).then((res) => {
+                                    setResult(res.data);
+                                    window.localStorage.setItem('searchedAlumni', JSON.stringify(res.data));
+
+                                  }).catch((err) => {
+                                    console.log(err)
+                                  })
+                                  navigate(`/comment/${val.name}/${val.roll_no}`)
+                                  
+                                  loadingSpinner2();
+                                }}><p>{val.name}</p>
+                                  <p style={{ fontSize: "70%", fontStyle: "italic" }}>{val.academic_program}</p>
+                                </button></li>)
+                                )}
+                              </ul>
+                            }
+                          </>}
+                        </div>
+
+                      </li>
+                    </div>
+                  }
+                </>
+              </ul>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <>
+        {/* <div className='navbar_phone absolute z-10'>
+      <motion.div className='flex flex-col justify-center items-center bg-[#180c1e] overflow-y-hidden'
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}>
+        <div className='h-screen w-screen bg-[#4d1a6c] flex flex-col items-center justify-start pt-8'>
+          <img src='/images/1.png' className='absolute ml-4 top-0 left-0 mt-4 w-16 h-24 rounded-xl' alt='err'></img>
+          <motion.div className='absolute top-0 right-0 mt-8 mr-8 text-3xl' onClick={() => setIsOpen(isOpen => !isOpen)}>×</motion.div>
+          {loggedin &&
+            <div id='loggedIn' className='mt-24 mb-16'>
+              <div className="searchr" style={{ width: '100%', display: "flex" }}>
+                {(isStudent || verified) && <>
+                  <input type="text" placeholder="Search..." class="search" onChange={(e) => {
+                    searchAWord(e);
+                    (e.target.value === "") ? setDisplay(false) : setDisplay(true);
+                    // onEnter();
+                  }} value={inputValue} />
+                  {wordEnteredList.length === 0 &&
+                    <ul className="dropdown-nav">
+                      <li><button className={`btnsearch2 ${(display) ? "" : "display-none"}`} style={{ textAlign: "left" }}>No User Found</button></li>
+                    </ul>}
+                  {wordEnteredList.length !== 0 &&
+                    <ul>
+                      {wordEnteredList.map((val, index) =>
+                      (<li><button className={`btnsearch2 ${(display) ? "" : "display-none"}`} style={{ textAlign: "left" }} key={index} onClick={(e) => {
+                        e.preventDefault();
+                        setSearchword(val.email);
+                        setInputValue("");
+                        setDisplay(false);
+                        e.target.value = "";
+                        setTimeout(() => {
+                          navigate(`/comment/${result[0]._id}/${result[0].name}/${result[0].roll_no}`)
+                        }, 1000)
+                      }}><p>{val.name}</p>
+                        <p style={{ fontSize: "70%", fontStyle: "italic" }}>{val.academic_program}</p>
+                      </button></li>)
+                      )}
+                    </ul>
+                  }
+                </>}
+              </div>
+            </div>
+          }
+          <a href='/'><div className={loggedin ? 'mb-12 uppercase' : 'uppercase mt-24 mb-24'}>Home</div></a>
+          <a href='/'><div className={loggedin ? 'mb-12 uppercase' : 'uppercase mb-24'}>about</div></a>
+          <a href='/'><div className={loggedin ? 'mb-12 uppercase' : 'uppercase mb-24'}>developers</div></a>
+          <a href='/'><div onClick={handleLogout} className={loggedin ? 'uppercase' : 'hidden'}>Logout</div></a>
+          <div className='google-login'>
+          </div>
+        </div>
+        <motion.div onClick={() => setIsOpen(isOpen => !isOpen)} className='w-full bg-[#180c1e] flex justify-center'><div className={isOpen ? 'hidden' : 'text-center -mt-2 pt-4 overflow-hidden w-12 h-12 text-xl rounded-full bg-[#4d1a6c]'}>∨</div></motion.div>
+      </motion.div>
+      </div> */}
+      </>
+    </>
+
+
+
+  )
+}
+
+
+export default Navbar;
