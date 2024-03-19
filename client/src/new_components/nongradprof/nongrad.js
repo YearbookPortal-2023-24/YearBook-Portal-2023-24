032a8039d39128735748757fb173d407e0c39486
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2024-03-19 23:48:40 +0530

    fixed UI bugs

--- client/src/new_components/nongradprof/nongrad.js
+++ client/src/new_components/nongradprof/nongrad.js
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./ng.css";
 
 const Nongrad = () => {
   return (
-    <div className="flex flex-col md:flex-row md:justify-center" style={{ maxWidth: '9500px', margin: '0 auto', marginTop: '40px', marginLeft:'150px' }}>
-      <div className="comm2">
+    <div className="w-screen h-screen bg-bg-white flex flex-col-reverse gap-y-4 md:flex-row justify-center items-center" >
+      <div className="comm2 ">
         <h1 id="cmtm" className="text-center mt-0">My Comments</h1>
+        <p className="text-lg mt-48">Start Commenting on other people to view your comments here. <a href="/" className="hover:underline">Comment Now</a></p>
       </div>
-      <div className="flex flex-col mx-4 md:mx-0 md:w-1/2 " style={{marginLeft: '60px'}}>
+      <div className="flex flex-col md:ml-12">
         <div className="name3 mt-4 md:mt-8">
           <h3>Name:</h3>
         </div>
