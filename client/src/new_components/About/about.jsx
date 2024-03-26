f976c3898c599483f1596b87078826860195f675
Samruddhi788 <125824587+Samruddhi788@users.noreply.github.com>
2024-03-26 17:40:40 +0530

    About Us page completed

--- client/src/new_components/About/about.jsx
+++ client/src/new_components/About/about.jsx
@@ -6,28 +6,30 @@ import img4 from './Login3.png';
 
 const About = () => {
   return (
-    <div className='bg-white flex flex-col items-center justify-center text-base md:text-3xl'>
-      <div className='flex flex-col items-center mt-2 md:mt-10 text-base md:text-3xl'>
+    <div className='bg-white flex flex-col  items-center justify-center text-base md:text-3xl'>
+      <div className='flex flex-col md:flex-row items-center mt-2 md:mt-10 text-base md:text-3xl'>
         <h4 className='mb-2'>STEP 1 :<br />
         Login using Sign-in with Google</h4>
-        <img src={img2} alt="" className='w-full md:w-72 h-48 md:h-56 object-cover rounded-lg mb-2' />
+        <img src={img2} alt="" className='w-full md:w-72 md:ml-[380px] h-48 md:h-56 object-cover rounded-lg mb-2' />
       </div>
-      <div className='flex flex-col items-center mt-2 md:mt-10 text-base md:text-3xl'>
-        <img src={img1} alt="" className='w-full md:w-72 h-48 md:h-56 object-cover rounded-lg mb-2' />
+      <div className='flex flex-col  md:flex-row items-center mb-[20px]  md:mt-10 text-base md:text-3xl'>
+       
         <h4 className='mb-2'>STEP 2 :<br />
         Click on the icon at the top left to explore various. <br /> Click on "Search People" to search for your friends' profiles.</h4>
+        <img src={img1} alt="" className='w-full mt-[20px] md:w-72 md:ml-20 h-48 md:h-56 object-cover rounded-lg mb-2' />
       </div>
-      <div className='flex flex-col items-center mt-2 md:mt-10 text-base md:text-3xl'>
+      <div className='flex flex-col md:flex-row items-center mt-[20px] md:mt-10 text-base md:text-3xl'>
         <h4 className='mb-2'>STEP 3 :<br />
         Search for your friends using their names or institute . <br /> You can also filter your search by batch or department
         </h4>
-        <img src={img3} alt="" className='w-full md:w-72 h-48 md:h-56 object-cover rounded-lg mb-2' />
+        <img src={img3} alt="" className='w-full md:w-72  md:ml-20 h-48 md:h-56 object-cover rounded-lg mb-2' />
       </div>
-      <div className='flex flex-col items-center mt-2 md:mt-10 text-base md:text-3xl'>
-        <img src={img4} alt="" className='w-full md:w-72 h-48 md:h-56 object-cover rounded-lg mb-2' />
+      <div className='flex flex-col md:flex-row items-center mt-2 md:mt-10 text-base md:text-3xl'>
+       
         <h4 className='mb-2'>STEP 4 :<br />
         Write comments on the profiles of your friends
         </h4>
+        <img src={img4} alt="" className='w-full md:w-72 md:ml-[160px] h-48  md:h-56 object-cover rounded-lg mb-2' />
       </div>
     </div>
   );
