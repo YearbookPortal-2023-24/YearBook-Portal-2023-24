3446464c63b33ca41fcd44c11786cf416435c6db
aditiganvir28 <aditi10328@gmail.com>
2024-04-21 00:58:44 +0530

    cloudinary updated

--- client/src/new_components/Fill_Details3/Fill_Details3.js
+++ client/src/new_components/Fill_Details3/Fill_Details3.js
@@ -247,7 +247,7 @@ function Fill3({ isDarkMode, setIsDarkMode }) {
 
     setWait(true);
     axios
-      .post("https://api.cloudinary.com/v1_1/dsdrcddr4/image/upload", formData)
+      .post("https://api-eu.cloudinary.com/v1_1/dnqlvxfuc/upload", formData)
       .then((res) => {
         setWait(false);
         setImageUrl(res.data.url);
